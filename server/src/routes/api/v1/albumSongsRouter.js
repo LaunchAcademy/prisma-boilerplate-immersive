0b8af4f8b19561f4b6a6bb6c84095388e082d603
dovakeen118 <kegillis118@gmail.com>
2023-08-21 14:23:40 -0400

    lay of the land and clean up

--- server/src/routes/api/v1/albumSongsRouter.js
+++ server/src/routes/api/v1/albumSongsRouter.js
@@ -1,24 +1,26 @@
-import express from "express"
-import prisma from "../../../prisma/prisma.js"
-import cleanUserInput from "../../../servies/cleanUserInput.js"
+import express from "express";
+import prisma from "../../../prisma/prisma.js";
+import cleanUserInput from "../../../services/cleanUserInput.js";
 
-const albumSongsRouter = new express.Router({ mergeParams: true })
+const albumSongsRouter = new express.Router({ mergeParams: true });
 
 albumSongsRouter.post("/", async (req, res) => {
-    const { body } = req
-    const cleanedFormData = cleanUserInput(body)
-    const { name, isCool, plays, description } = cleanedFormData
-    const albumId = parseInt(req.params.albumId)
+  const { body } = req;
+  const cleanedFormData = cleanUserInput(body);
+  const { name, isCool, plays, description } = cleanedFormData;
+  const albumId = parseInt(req.params.albumId);
 
-    try {
-        const newSong = await prisma.song.create({ data: { name, isCool, plays, description, albumId }})
+  try {
+    const newSong = await prisma.song.create({
+      data: { name, isCool, plays, description, albumId },
+    });
 
-        return res.status(201).json({ song: newSong })
-    } catch (error) {
-        console.log(error)
-   
-        return res.status(500).json({ errors: error })
-    }
-})
+    return res.status(201).json({ song: newSong });
+  } catch (error) {
+    console.log(error);
 
-export default albumSongsRouter
+    return res.status(422).json({ errors: error });
+  }
+});
+
+export default albumSongsRouter;
--- server/src/routes/api/v1/albumsRouter.js
+++ server/src/routes/api/v1/albumsRouter.js
@@ -1,40 +1,40 @@
 import express from "express";
-import prisma from "../../../prisma/prisma.js"
-import albumSongsRouter from "./albumSongsRouter.js"
+
+import prisma from "../../../prisma/prisma.js";
+
+import albumSongsRouter from "./albumSongsRouter.js";
 
 const albumsRouter = new express.Router();
 
-albumsRouter.use("/:albumId/songs", albumSongsRouter)
+albumsRouter.use("/:albumId/songs", albumSongsRouter);
 
 albumsRouter.get("/", async (req, res) => {
-    try {
-        const albums = await prisma.album.findMany()
+  try {
+    const albums = await prisma.album.findMany();
 
-        return res.status(201).json({ albums });
-    } catch (error) {
-        console.log(error);
-        return res.status(422).json({ errors: error });
-    }
+    return res.status(200).json({ albums });
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({ errors: error });
+  }
 });
 
 albumsRouter.get("/:id", async (req, res) => {
-    try {
-        const album = await prisma.album.findUnique({
-            where: {
-                id: parseInt(req.params.id)
-            },
-            include: {
-                songs: true
-            }
-        })
+  try {
+    const album = await prisma.album.findUnique({
+      where: {
+        id: parseInt(req.params.id),
+      },
+      include: {
+        songs: true,
+      },
+    });
 
-        
-
-        return res.status(201).json({ album });
-    } catch (error) {
-        console.log(error);
-        return res.status(422).json({ errors: error });
-    }
+    return res.status(200).json({ album });
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({ errors: error });
+  }
 });
 
 export default albumsRouter;
