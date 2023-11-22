a2658fa39b205b2a73fde5e123a0357484aa455b
dovakeen118 <kegillis118@gmail.com>
2023-11-22 15:03:52 -0500

    remove passport for boilerplate

--- server/src/prisma/prisma.js
+++ server/src/prisma/prisma.js
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+import yupValidationPrismaClient from "./config/yupValidationPrismaClient.js";
+
+const prisma = new PrismaClient().$extends(yupValidationPrismaClient);
 
 export default prisma;
